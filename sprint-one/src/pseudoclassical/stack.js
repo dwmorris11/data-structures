b6ef06874719aa97fbd6f93c3d9c496f770b8a6d
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 09:30:34 -0500

    Updated through working pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,8 +3,19 @@ var Stack = function() {
   this.storageSize = 0;
 };
 
-Stack.prototype.push = function (value) {};
-Stack.prototype.pop = function () {};
+Stack.prototype.push = function (value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+Stack.prototype.pop = function () {
+  var key = String(this.storageSize-1);
+  if(this['storage'].hasOwnProperty(key)) {
+    var poppedValue = this.storage[key];
+    delete this.storage[key];
+    this.storageSize--;
+  }
+  return poppedValue;
+};
 Stack.prototype.size = function () {
   return this.storageSize;
 };
