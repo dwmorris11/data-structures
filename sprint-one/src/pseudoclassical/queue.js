8783fd2b416015e4a4cf9b4a40edac329e4c6bcd
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 09:35:45 -0500

    Updated through pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,8 +3,27 @@ var Queue = function() {
   this.storageSize = 0;
 };
 
-Queue.prototype.enqueue = function (value) {};
-Queue.prototype.dequeue = function () {};
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+Queue.prototype.dequeue = function () {
+  if (this.storage['0'] === undefined) { return; }
+  var dequeuedValue = this.storage['0'];
+  delete this.storage['0'];
+  var storageClone = _.clone(this.storage);
+  this.storage = {};
+  _.each(storageClone, function (item, key) {
+    key = Number(key) - 1;
+    if(this.storage === undefined){
+      debugger;
+    }
+    this.storage[String(key)] = item;
+  }, this);
+  this.storageSize--;
+  return dequeuedValue;
+};
 Queue.prototype.size = function () {
   return this.storageSize;
 };
+
