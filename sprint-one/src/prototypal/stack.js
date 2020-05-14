32d1b66d4af8023530c24341b7b7b8c123518e5c
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 09:12:54 -0500

    Completed through prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,11 +1,25 @@
 var Stack = function() {
-  var someInstance = Object.create(Stack.prototype);
+  var someInstance = Object.create(stackMethods);
   someInstance.storage = {};
   someInstance.storageSize = 0;
   return someInstance;
 };
 
-Stack.prototype.push = function () {};
-Stack.prototype.pop = function () {};
-Stack.prototype.size = function () {};
+var stackMethods = {};
+stackMethods.push = function (value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+stackMethods.pop = function () {
+  var key = String(this.storageSize-1);
+  if(this['storage'].hasOwnProperty(key)) {
+    var poppedValue = this.storage[key];
+    delete this.storage[key];
+    this.storageSize--;
+  }
+  return poppedValue;
+};
+stackMethods.size = function () {
+  return this.storageSize;
+};
 
