9c8433e38c85c0eeb8caec06778dffd5b2b90993
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 08:55:30 -0500

    Built skeleton for prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,11 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(Stack.prototype);
+  someInstance.storage = {};
+  someInstance.storageSize = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
-
+Stack.prototype.push = function () {};
+Stack.prototype.pop = function () {};
+Stack.prototype.size = function () {};
 
