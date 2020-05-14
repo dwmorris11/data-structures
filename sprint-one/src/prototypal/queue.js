9254b8fad5b792e30dab1cf93884b2c0fc16894c
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 09:20:22 -0500

    Updated and working through prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,8 +7,28 @@ var Queue = function() {
 
 var queueMethods = {};
 
-queueMethods.enqueue = function (value) {};
-queueMethods.dequeue = function () {};
-queueMethods.size = function () {this.storageSize;};
+queueMethods.enqueue = function (value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+queueMethods.dequeue = function () {
+  if (this.storage['0'] === undefined) { return; }
+  var dequeuedValue = this.storage['0'];
+  delete this.storage['0'];
+  var storageClone = _.clone(this.storage);
+  this.storage = {};
+  _.each(storageClone, function (item, key) {
+    key = Number(key) - 1;
+    if(this.storage === undefined){
+      debugger;
+    }
+    this.storage[String(key)] = item;
+  }, this);
+  this.storageSize--;
+  return dequeuedValue;
+};
+queueMethods.size = function () {
+  return this.storageSize;
+};
 
 
