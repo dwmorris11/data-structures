81b7abb38987da8c85f6db5565a686153b69c54c
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 09:16:06 -0500

    Setup skeleton for protypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,14 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.storageSize = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (value) {};
+queueMethods.dequeue = function () {};
+queueMethods.size = function () {this.storageSize;};
 
 
