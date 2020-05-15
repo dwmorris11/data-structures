254340b09dd3f96a9cc6aa3cb8db008af5df021c
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 23:45:39 -0500

    All functions working on tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,40 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  var result = false;
+  if (this.value === target) {
+    return true;
+  }
+  if (this.children.length > 0) {
+    result = _.some(this.children, function(node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        return _.some(node.children, function(nod) {
+          if (nod.value === target) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
+    });
+  }
+  return result;
 };
+
 
 
 
