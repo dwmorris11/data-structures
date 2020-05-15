f5328f0ad5954f3df5001ef28e336797bf29ef23
Dustin Morris <dustinnuc@gmail.com>
2020-05-15 17:42:42 -0500

    Completed all functions of set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//Note: Sets should not use up any more space than necessary. Once a value is added to a set, adding it a second time should not increase the size of the set.
