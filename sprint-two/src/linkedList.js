dc4aca37e6005d82389440bd74de5136d2924398
Dustin Morris <dustinnuc@gmail.com>
2020-05-14 20:39:12 -0500

    Updated and working LinkedList through addTail and removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,43 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.size = 0;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (list.size === 0) {
+      list.head = node;
+      list.head.next = node;
+    }
+    if (list.size === 1) {
+      list.head.next = node;
+    }
+    if (list.tail) {
+      list.tail.next = node;
+    }
+    list.tail = node;
+    list.size++;
   };
 
   list.removeHead = function() {
+    var oldHeadValue = list.head.value;
+    list.head = list.head.next;
+    return oldHeadValue;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    var onceMore = true;
+    do {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+      if (currentNode.next === null) {
+        onceMore = false;
+      }
+    }
+    while (currentNode.next !== null && onceMore);
   };
 
   return list;
