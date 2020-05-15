6a6f99e8a16a8cfd3b39c8e4e128a2af9805d259
Dustin Morris <dustinnuc@gmail.com>
2020-05-15 16:59:10 -0500

    Working up to add/has edge;

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,64 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push([node]);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.some(this.nodes, function(nod) {
+    if(nod[0] === node) {
+      return true;
+    }
+  });
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  //array of nodes [[node1, node2, node3], [node2], ....]
+  // iterate over each sub-array
+  for (var i = 0; i < this.nodes.length; i++) {
+    // look at index 0 of each sub-array and compare it to node
+    if (this.nodes[i][0] === node) {
+      // Remove the node
+      this.nodes.splice(i, 1);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // Iterate over the nodes and find fromNode
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i][0] === fromNode) {
+      if (this.nodes[i].includes(toNode)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // Iterate over the nodes, look for fromNode
+  for (var i = 0; i < this.nodes.length; i++) {
+    // Push toNode to fromNode
+    if (fromNode === this.nodes[i][0]) {
+      this.nodes[i].push(toNode);
+    }
+  }
+  // Iterate over the nodes, look for toNode
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (toNode === this.nodes[i][0]) {
+      this.nodes[i].push(fromNode);
+    }
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
